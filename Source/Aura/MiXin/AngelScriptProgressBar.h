22d2e3009bc583a9d786a2d2b519946bd26543a9
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-10 13:40:00 +0800

    暴露 UProgressBar::SetWidgetStyle 接口 给 脚本

---
+++ Source/Aura/MiXin/AngelScriptProgressBar.h
@@ -0,0 +1,23 @@
+﻿// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ProgressBar.h"
+#include "UObject/Object.h"
+#include "AngelScriptProgressBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Meta = (ScriptMixin = "UProgressBar"))
+class AURA_API UAngelScriptProgressBar : public UObject
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(ScriptCallable)
+	static void SetWidgetStyle(UProgressBar* ProgressBar, FProgressBarStyle& InStyle)
+	{
+		ProgressBar->SetWidgetStyle(InStyle);
+	}
+};
